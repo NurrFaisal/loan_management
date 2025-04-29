13ee00f9f343b44d74be4cf73aa349c354b101a0
NurFaisal <noormoy@gmail.com>
2025-04-29 21:46:40 +0600

    added model and migration

---
+++ database/migrations/2025_04_29_153422_create_loans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('somitee_id');
+            $table->unsignedInteger('member_id');
+            $table->bigInteger('loan_amount');
+            $table->tinyInteger('interest');
+            $table->bigInteger('total_loan');
+            $table->string('type');
+            $table->integer('installment');
+            $table->integer('installment_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};
---
+++ database/migrations/2025_04_29_153923_create_insurances_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('insurances', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedInteger('somitee_id');
+            $table->unsignedInteger('member_id');
+            $table->string('type');
+            $table->integer('installment');
+            $table->bigInteger('total_amount');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('insurances');
+    }
+};
